07617327ac115efd09eca8047079e055abf1fcf4
Zaya <heavenphrince@gmail.com>
2024-01-07 17:22:00 +0100

    pascal triangle solved-hopefully

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" module for implementation of Pascal's triangle
+"""
+
+def pascal_triangle(n):
+    """ creates a pascal trinagle
+    """
+
+    if n <= 0:
+        return []
+
+    pt = [0] * n
+ 
+    for x in range(n):
+        new_row = [0] * (x + 1)
+        new_row[0] = 1
+        new_row[-1] = 1
+
+        for y in range(1, x):
+            if y > 0 and y < len(new_row):
+                a = pt[x - 1][y]
+                b = pt[x - 1][y - 1]
+                new_row[y] = a + b
+
+        pt[x] = new_row
+
+    return pt
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+An implementation of pascal triangle. Provided a positive integer, it generates a the pascal triangle terminating at the nth+1 row
